714f08e20e6bfad78f3bd9fe6761cade84e3f052
Elsedin <elsedin.kesko@edu.fit.ba>
2026-03-01 15:53:30 +0100

    Remove sensitive files, update .gitignore and fix backend configuration for emulator access

--- user-mobile/lib/config/app_config.dart
+++ user-mobile/lib/config/app_config.dart
@@ -1,3 +1,22 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+
 class AppConfig {
-  static const String apiBaseUrl = 'http://localhost:5178/api';
+  static const int apiPort = 5178;
+  
+  static String get apiBaseUrl {
+    if (kIsWeb) {
+      return 'http://localhost:$apiPort/api';
+    }
+    
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:$apiPort/api';
+    }
+    
+    if (Platform.isIOS) {
+      return 'http://localhost:$apiPort/api';
+    }
+    
+    return 'http://localhost:$apiPort/api';
+  }
 }
